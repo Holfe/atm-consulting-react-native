6b7fd4aeaf9c1090d20b09d9d02cbbb4cb6804ad
Holfe <wvcneto@gmail.com>
2019-10-15 19:12:39 -0300

    Start Project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import Navbar from './src/components/Navbar';
+
+export default function App() {
+  return (
+    <Navbar />
+  );
+}
